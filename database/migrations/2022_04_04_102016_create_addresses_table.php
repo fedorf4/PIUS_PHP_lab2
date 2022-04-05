7b997c70e24a735885f9455680ae05c9ea06493a
fedorf4 <79131391+fedorf4@users.noreply.github.com>
2022-04-05 22:46:34 +0300

    created migrations

---
+++ database/migrations/2022_04_04_102016_create_addresses_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_from_customer');
+            $table->string('city');
+            $table->string('street_or_district');
+            $table->integer('house_number');
+            $table->integer('floor')->nullable();
+            $table->integer('flat_number');
+            $table->string('intercom_code')->nullable();
+            $table->foreignId('customer_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
