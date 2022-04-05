461b4f89b84bf4550c6a3977252d73c6a9b3fa43
fedorf4 <79131391+fedorf4@users.noreply.github.com>
2022-04-05 22:45:39 +0300

    created seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Database\Factories\CustomerFactory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            CustomerSeeder::class,
+            AddressSeeder::class
+        ]);
     }
 }
