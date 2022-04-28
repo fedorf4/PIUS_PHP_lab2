d084986c4fb242573310485446c67e9fe6bd5a40
fedorf4 <fedor_fomichev@list.ru>
2022-04-28 17:30:17 +0300

    prepare for lab3

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/customers', [CustomerController::class, 'showCustomersFiltered']);
-
-Route::get('/customers/{id}', [CustomerController::class, 'showCertainCustomerAndAdresses']);
